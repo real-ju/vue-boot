f3434a0eb53ac581ac98dfd8806c03cf9026f0db
ju <2201181566@qq.com>
2021-04-04 22:08:10 +0800

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,33 +7,35 @@ module.exports = {
     extends: [
         'eslint:recommended',
         'plugin:vue/essential',
-        'plugin:prettier/recommended',
+        // 'plugin:prettier/recommended',
     ],
     parserOptions: {
         ecmaVersion: 12,
         sourceType: 'module'
     },
     plugins: [
-        'prettier'
+        // 'prettier'
     ],
     rules: {
-        "prettier/prettier": ["error"],
-        // "no-multiple-empty-lines": ["off"],
-        // "space-before-function-paren": ["error", "never"],
-        // "indent": ["error", 4, {
-        //     "MemberExpression": "off"
-        // }],
-        // "prefer-const": ["off"],
-        // "keyword-spacing": ["error", {
-        //     "overrides": {
-        //         "if": { "after": false },
-        //         "for": { "after": false },
-        //         "while": { "after": false }
-        //     }
-        // }],
-        // "semi": ["off"],
-        // "brace-style": ["error", "stroustrup"],
-        // "no-trailing-spaces": ["off"],
-        // "eol-last": ["off"],
+        // "prettier/prettier": ["error"],
+        "no-multiple-empty-lines": ["off"],
+        "space-before-function-paren": ["error", "never"],
+        "indent": ["error", 4, {
+            "MemberExpression": "off"
+        }],
+        "prefer-const": ["off"],
+        "keyword-spacing": ["error", {
+            "overrides": {
+                "if": { "after": false },
+                "for": { "after": false },
+                "while": { "after": false }
+            }
+        }],
+        "semi": ["off"],
+        "brace-style": ["error", "stroustrup"],
+        "no-trailing-spaces": ["off"],
+        "eol-last": ["off"],
+        "no-unused-vars": ["off"],
+        "no-undef": ["off"],
     }
 }
